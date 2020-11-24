1f2fa0f3187dff62d794026334fe6cec1ffc408c
Brian J. Murrell <brian.murrell@intel.com>
2020-11-24 13:50:41 -0500

    Aesthetics
    Skip-checkpatch: true
    Skip-build: true
    Skip-unit-tests: true
    Skip-coverity-test: true
    Skip-func-test-el7: true
    Skip-func-hw-test: true
    PR-repos: libfabric@PR-33:2
    RPM-test-version: 1.1.1-6.5297.g61ce3f88
    Test-tag: pr,-hw,sync_poolattribute
    Skip-scan-centos-rpms: true
    Skip-test-centos-rpms: true
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>

--- ci/provisioning/post_provision_config_nodes_EL_7.sh
+++ ci/provisioning/post_provision_config_nodes_EL_7.sh
@@ -20,12 +20,12 @@ post_provision_config_nodes() {
     if [ -n "$DAOS_STACK_GROUP_REPO" ]; then
          rm -f /etc/yum.repos.d/*"$DAOS_STACK_GROUP_REPO"
          yum-config-manager \
-             --add-repo="$REPOSITORY_URL$DAOS_STACK_GROUP_REPO"
+             --add-repo="${REPOSITORY_URL}${DAOS_STACK_GROUP_REPO}"
     fi
 
     if [ -n "$DAOS_STACK_LOCAL_REPO" ]; then
         rm -f /etc/yum.repos.d/*"$DAOS_STACK_LOCAL_REPO"
-        yum-config-manager --add-repo="$REPOSITORY_URL$DAOS_STACK_LOCAL_REPO"
+        yum-config-manager --add-repo="${REPOSITORY_URL}${DAOS_STACK_LOCAL_REPO}"
         echo "gpgcheck = False" >> \
             /etc/yum.repos.d/*"${DAOS_STACK_LOCAL_REPO//\//_}".repo
     fi
